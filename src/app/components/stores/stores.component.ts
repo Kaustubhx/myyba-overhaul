db1f7af39466a3637d4e40a9210bf8d2f7d21a0e
Kastubhx <prime.ganekar@gmail.com>
2022-11-02 19:17:35 +0530

    New: Changed Views per slide

--- src/app/components/stores/stores.component.ts
+++ src/app/components/stores/stores.component.ts
@@ -10,11 +10,36 @@ export class StoresComponent implements OnInit {
 
   constructor(private storeDetail: DataService) { }
 
-  stores: any 
+  stores: any
 
 
   ngOnInit(): void {
     this.stores = this.storeDetail.storeImages
     console.log(this.stores)
   }
+
+  slideOpts = {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 0
+      }
+    }
+  };
 }
